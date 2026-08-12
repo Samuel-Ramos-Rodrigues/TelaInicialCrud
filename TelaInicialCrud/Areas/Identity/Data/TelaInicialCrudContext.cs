4bc3c612a42f0e9813a2c2ffea333e0149f28029
Samuel-Ramos-Rodrigues <samueltulioramos222@gmail.com>
2026-08-12 16:56:35 -0300

    Adicionar arquivos de projeto.

---
+++ TelaInicialCrud/Areas/Identity/Data/TelaInicialCrudContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TelaInicialCrud.Areas.Identity.Data;
+using TelaInicialCrud.Models;
+
+namespace TelaInicialCrud.Areas.Identity.Data;
+
+public class TelaInicialCrudContext : IdentityDbContext<Usuario>
+{
+
+    public TelaInicialCrudContext(DbContextOptions<TelaInicialCrudContext> options)
+        : base(options)
+
+    {
+    }
+
+    public DbSet<Projeto> Projeto { get; set; }
+
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ConfigUser());
+    }
+}
+
+
+public class ConfigUser : IEntityTypeConfiguration<Usuario>
+{
+    public void Configure(EntityTypeBuilder<Usuario> builder)
+    {
+        builder.Property(x => x.Nome).HasMaxLength(150);
+        builder.Property(x => x.Sobrenome).HasMaxLength(150);
+    }
+}
---
+++ TelaInicialCrud/Models/Projeto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace TelaInicialCrud.Models
+{
+    public class Projeto
+    {
+        [Key]
+        public int IdProjeto { get; set; }
+        public string NomeProjeto { get; set; }
+        public string Descricao { get; set; }
+
+        public float valor { get; set; }
+
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFinal { get; set; }
+        public int QuantPessoas { get; set; }
+    }
+}
